b270f0f6584381ffa34feafbb2f1992ca4d32d49
Sai rishi Kumar Reddy Reddy <saiboggadi481@gmail.com>
2025-02-03 23:03:50 +0530

    day 1

---
+++ src/kitten_FC/NewClass.java
@@ -0,0 +1,17 @@
+package kitten_FC;
+
+public class NewClass {
+	public static void main(String[] args) {
+		System.out.println("I am  a main methods ");	
+		methodUserRep();
+		
+	}
+	
+	public static void methodUserRep ()
+	{
+		System.out.println("I am a method of User Rep");
+	}
+
+}
+
+
